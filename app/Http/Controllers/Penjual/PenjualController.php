570231afc4bff39dcd6f9d66aee7d868aa10c234
COBAPC\naufan <naufanarifie@gmail.com>
2017-05-17 04:23:06 +0700

    das

--- app/Http/Controllers/Penjual/PenjualController.php
+++ app/Http/Controllers/Penjual/PenjualController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers\Penjual;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Transaksi;
+use App\TransaksiDetail;
+
 
 class PenjualController extends Controller
 {
     public function index($id)
     {
+			$transaksi = DB::table('transaksi')->where('penjual', $id)->get();
+			//foreach ($transaksi as $tmp)
+			//{
+				//echo ($tmp->id);
+			//}
+		
 			$data['nama'] = $this->getName($id); 
+			
     		return view('penjual.lek-e',$data);
     }
 	
@@ -19,6 +30,15 @@ class PenjualController extends Controller
 		$nama = User::find($id);
     	return $nama;
 	}
-	
+	private function getOrderan()
+	{
+		$antrian = Transaksi::where([
+    			['status',0],
+    			['accepted_at','<>',NULL],
+    			['penjual',$id],
+    			['id','<',$transaksi]
+    			])
+    		->get();
+	}
 
 }
